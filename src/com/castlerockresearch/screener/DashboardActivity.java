d4c65835fe5cf74e621e3259d6ea85c52c73c58a
Kumar Bibek <kbibek@crri.co.in>
2011-04-01 19:14:37 +0530

    Initial commit

---
+++ src/com/castlerockresearch/screener/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.castlerockresearch.screener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.castlerockresearch.screencapture.R;
+
+public class DashboardActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
